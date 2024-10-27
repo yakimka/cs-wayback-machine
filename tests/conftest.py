a6969c0d6f1e3db04650d5904018509ca5d7c8ca
yakimka <ss.yakim@gmail.com>
2024-10-27 21:31:35 +0200

    Update settings

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,13 @@
+import pytest
+
+from cs_wayback_machine.settings import settings
+
 pytest_plugins = [
     "picodi.integrations._pytest",
     "picodi.integrations._pytest_asyncio",
 ]
+
+
+@pytest.fixture(scope="session", autouse=True)
+def _set_test_settings():
+    settings.configure(FORCE_ENV_FOR_DYNACONF="testing")
